ddfe8d5cebfe43ac15b4d42a9d47e9e0754d06ce
jvictor977 <j.v.abreu97@gmail.com>
2024-09-06 16:50:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        titulo: "GSX-S 750",
+        descricao: "Suzuki GSX-S 750 A Naked Sport para todos os gostos, é uma motocicleta naked sport que combina desempenho, estilo e versatilidade.Com seu motor de quatro cilindros em linha de 750cc, ela oferece uma experiência de pilotagem emocionante e ágil, tanto na cidade quanto em estradas mais sinuosas.",
+        link: "https://www.motonline.com.br/guia-de-motos/suzuki/gsx-s-750",
+        imagem: "https://espacomotosdf.com.br/wp-content/uploads/2023/02/GSX-S750-preta-45o-ofertas.png",
+        tags: "gsx suzuki gsx-s 750"
+    },
+    {
+        titulo: "MT-09",
+        descricao: "A Yamaha MT-09 é uma naked esportiva que combina um design agressivo com um motor de três cilindros em linha que entrega torque e potência em baixas e médias rotações, proporcionando uma experiência de pilotagem emocionante e divertida.",
+        link: "https://www.yamaha-motor.com.br/mt-09-abs/product/30120",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF7hz7x8D-EcCt4QintXyql0Ua-PDmJb4x_A&s",
+        tags: "mt09 mt-09 yamaha"
+    },
+    {
+        titulo: "Speed Twin 1200",
+        descricao: "A Triumph Speed Twin 1200 é uma motocicleta clássica com um toque moderno, oferecendo um motor bicilíndrico em linha de 1200cc que entrega um torque forte e linear, proporcionando uma pilotagem suave e prazerosa.",
+        link: "https://www.triumphmotorcycles.com.br/motocicletas/classic/bonneville-speed-twin-1200",
+        imagem: "https://img.stcrm.it/images/34570200/1000x/speed-twin-1200-my24-phantom-red-angle-rhs_risultato-jpg.jpg",
+        tags: "speed triumph speed twin 1200"
+    },
+    {
+        titulo: "Street Triple 765 RS",
+        descricao: "A Triumph Street Triple 765 RS é uma naked esportiva de alta performance, com um motor tricilíndrico de 765cc que oferece uma potência incrível e um manuseio preciso, tornando-a uma moto perfeita para quem busca emoção nas ruas.",
+        link: "https://www.triumphmotorcycles.com.br/motocicletas/roadsters/street-triple-765?gad_source=1&gclid=Cj0KCQjwiuC2BhDSARIsALOVfBK0UD-iryXngVUOCLkDLqDkP1JzKeUp928s_tNkWueK62fQd2K50rcaArKUEALw_wcB",
+        imagem: "https://media.triumphmotorcycles.co.uk/image/upload/q_auto/sitecoremedialibrary/media-library/images/motorcycles/roadsters-supersports/my23/street%20triple/765%20rs%20variant/colours/carnival%20red/my23-street-triple-rs-carnival-red-rhs-825x472.png",
+        tags: "street triple triumph 765"
+    },
+    {
+      titulo: "Honda CB 500X",
+      descricao: "A Honda CB 500X 2023 chega ao Brasil com um design mais aventureiro e motor bicilíndrico de 500cc, ideal para o dia a dia e viagens curtas.",
+      link: "https://www.honda.com.br/motocicletas/cb500x",
+      imagem: "https://i0.wp.com/rodaomotos.com.br/wp-content/uploads/2020/09/Honda_500X_prata.png?w=860&ssl=1",
+      tags: "honda cb500x cb500"
+    },
+    {
+        titulo: "Yamaha MT-07",
+        descricao: "A Yamaha MT-07 2023 chega ao Brasil com um design mais agressivo e motor bicilíndrico de 689cc, ideal para quem busca uma moto versátil e divertida.",
+        link: "https://www.yamaha-motor.com.br/mt-07-abs/product/30119?srsltid=AfmBOoqWcqhNvj1QUjtOrT_5ZQ72ouKFFOnA1gkcEFMF5FhMSb4duuhR",
+        imagem: "https://www.yamaha-motor.com.br/ccstore/v1/images/?source=/file/v5457449167124694502/products/30119.301191.png&height=300&width=300",
+        tags: "yamaha mt07 mt mt-07"
+      },
+      {
+        titulo: "BMW G 310 GS",
+        descricao: "A BMW G 310 GS é uma trail leve e ágil, perfeita para quem busca uma moto para o dia a dia e aventuras leves.",
+        link: "https://www.bmw-motorrad.com.br/pt/models/adventure/g310gs.html",
+        imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTSUDI9I3ceUoCzg7ls2c4A6jeAxAcI6odPQ&s",
+        tags: "bmw g310 310gs "
+      }
+    ];
+  
